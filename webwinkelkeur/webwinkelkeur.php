7d37476d2957d75fc4bcac6a76523973c9c97f16
Albert Peschar <albert@peschar.net>
2013-12-18 10:22:51 +0100

    fix Plugin URI

--- webwinkelkeur/webwinkelkeur.php
+++ webwinkelkeur/webwinkelkeur.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Webwinkelkeur
-Plugin URI: https://www.webwinkelkeur.nl/webwinkel/mogelijkheden/wordpress-plugin/
+Plugin URI: https://www.webwinkelkeur.nl/webwinkel/mogelijkheden/wordpress-module/
 Description: De WordPress plugin zorgt voor een eenvoudige integratie van het WebwinkelKeur binnen jouw webwinkel. Hiermee is het heel eenvoudig om de innovatieve <a href="https://www.webwinkelkeur.nl/webwinkel/mogelijkheden/sidebar/">WebwinkelKeur Sidebar</a> in jouw WordPress website of WooCommerce webwinkel te integreren. Wanneer je WooCommerce gebruikt, kunnen er bovendien automatisch uitnodigingen naar je klanten worden gestuurd.
 Version: 1.0
 Author: Albert Peschar
