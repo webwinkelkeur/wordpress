754ec2f72623df8feda79318e4d95673d4370b64
Albert Peschar <albert@peschar.net>
2017-05-30 11:03:14 +0200

    Bump plugin versions to 1.4.0

--- evalor/evalor.php
+++ evalor/evalor.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: eValor
 Plugin URI: https://www.evalor.es/tienda/modulo/wordpress/
-Description: El módulo WordPress facilita la integración del innovador sidebar de evalor en su web WordPress o tienda WooCommerce.  Para los usuarios del WooCommerce Plugin para WordPress el módulo se encarga de invitar de forma automática a los clientes para que compartan su experiencia. Esta funcion solo esta disponible para clientes PLUS.  Cuando el proceso del pedido ha finalizado se envia automaticamente una invitacion al cliente para que comparta su experiencia. Asi se genera confianza y aumenta la conversion de su tienda online. 
-Version: 1.1.2
+Description: El módulo WordPress facilita la integración del innovador sidebar de evalor en su web WordPress o tienda WooCommerce.  Para los usuarios del WooCommerce Plugin para WordPress el módulo se encarga de invitar de forma automática a los clientes para que compartan su experiencia. Esta funcion solo esta disponible para clientes PLUS.  Cuando el proceso del pedido ha finalizado se envia automaticamente una invitacion al cliente para que comparta su experiencia. Asi se genera confianza y aumenta la conversion de su tienda online.
+Version: 1.4.0
 Author: Albert Peschar
 Author URI: https://peschar.net/
 */
