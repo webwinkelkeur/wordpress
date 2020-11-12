5f76958ae99ef4748f7dd3e28982cbcbe6fb47da
TsankoBg <tsankohadzhiev@gmail.com>
2020-11-12 19:13:44 +0200

    Add product reviews (#5)
    * Remove screen_icon()
    
    * Wip: add product review tab in settings
    
    * Add product_url to order_data
    
    * Change product_reviews config to boolval
    
    * Update product_reviews text
    
    * Add gtin option to product
    
    * Rename gtin_code field
    
    * WIP: standardise products data
    
    * Change array() to []
    
    * Remove gtin field in product options
    
    * Fix translations
    
    * Code improvements
    
    * Fix error in getProductImage
    
    * Rename WP_Product to WC_Product
    
    * WIP: get gtin from actie plugin
    
    * WIP: get GTIN from active plugin
    
    * WIP: add most popular G feed plugins support
    
    * Extract gtin from active plugin
    
    * Add type hints
    
    * Chnage label description
    
    * Add 'webwinkelkeur' parameter to __
    
    * Formatting
    
    * Restore spacing
    
    * Restore formatting
    
    * Restore formatting 2
    
    * Fix spacing
    
    * Code improvements
    
    * More code improvements
    
    * Fix condition for showing gtin field
    
    * Fix indentation
    
    * Get single value from gtin metadata
    
    * WIP: use single array for supported plugins
    
    * Formatting
    
    * WIP: add most popular G feed plugins support
    
    * Remove 'code' after GTIN
    
    * Fix hasActivePlugin
    
    * Add getGtinMetaKey to BasePlugin
    
    * Pass $plugin to GtinHandler
    
    * Remove product from constructor
    
    * Work in progress- import reviews
    
    * WIP: remove staging credentials
    
    * Add scheduled event for syncing reviews
    
    * Minor improvemetns on CommentData
    
    * CommentData improvements and rename functions
    
    * Remove empty line
    
    * Remove unnecessary function getProductItem
    
    * Skip reviews for deleted products
    
    * Return reviews in SimpleXML
    
    * Parse reviews from SimpleXML
    add prefix for review's cron
    
    * Get review from last_synced date and code improvements
    
    * Get review_id meta key from function
    
    * Fix logical operator
    
    * Handle empty phone number
    
    * Disable review sync when Woocommerce not installed
    
    * Fxi typehinting
    
    * Move getGtinKey to WooCommerce.php

---
+++ common/src/GtinHandler.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Valued\WordPress;
+
+class GtinHandler {
+    const SUPPORTED_PLUGINS = [
+        'woocommerce-product-feeds/woocommerce-gpf.php' => null,
+        'customer-reviews-woocommerce/ivole.php' => '_cr_gtin',
+        'product-gtin-ean-upc-isbn-for-woocommerce/product-gtin-ean-upc-isbn-for-woocommerce.php' => '_wpm_gtin_code',
+        'woo-product-feed-pro/woocommerce-sea.php' => '_woosea_gtin',
+    ];
+    private $product;
+    private $gtin_meta_key;
+
+    public function __construct(string $gtin_meta_key) {
+        $this->gtin_meta_key = $gtin_meta_key;
+    }
+
+    public function setProduct(\WC_Product $product): void {
+        $this->product = $product;
+    }
+
+    public function hasActivePlugin(): bool {
+        foreach (self::SUPPORTED_PLUGINS as $plugin_name => $key) {
+            if (is_plugin_active($plugin_name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function getGtin(): ?string {
+        if (is_plugin_active('woocommerce-product-feeds/woocommerce-gpf.php')) {
+            return $this->handleGpf();
+        }
+        return $this->getGtinFromMeta($this->getGtinMetaKey());
+    }
+
+    private function getGtinMetaKey(): string {
+        foreach (array_filter(self::SUPPORTED_PLUGINS, 'strlen') as $plugin_name => $key) {
+            if (is_plugin_active($plugin_name)) {
+                return $key;
+            }
+        }
+        return $this->gtin_meta_key;
+    }
+
+    private function getGtinFromMeta(string $key): ?string {
+        return (string) get_post_meta($this->product->get_id(), $key, true) ?: null;
+    }
+
+    private function handleGpf(): ?string {
+        return (string) woocommerce_gpf_show_element('gtin', $this->product->post) ?: null;
+    }
+}
