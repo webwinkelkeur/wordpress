2a3dbb458149be040b195f9ab1adba102d3b81bb
tsanko <tsankohadzhiev@gmail.com>
2020-11-16 13:01:38 +0200

    Remove old API checks

--- common/src/API.php
+++ common/src/API.php
@@ -50,12 +50,6 @@ class API {
         if (isset($result->status) && $result->status == 'success') {
             return true;
         }
-        if (preg_match('|already sent|', $result->message)) {
-            throw new WebwinkelKeurAPIAlreadySentError($url, $result->message);
-        }
-        if (preg_match('|limit hit|', $result->message)) {
-            throw new WebwinkelKeurAPIAlreadySentError($url, $result->message);
-        }
         throw new WebwinkelKeurAPIError(
             $url,
             isset($result->message) ? $result->message : $response->body
