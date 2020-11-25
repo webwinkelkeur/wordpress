89ab1a60d43358ca5d64a75a9ed3bd7425a66c51
Albert Peschar <albert@peschar.net>
2020-11-25 10:11:37 +0000

    Clean up PhpCompatibilityCheck

--- common/src/PhpCompatibilityCheck.php
+++ common/src/PhpCompatibilityCheck.php
@@ -1,23 +1,22 @@
 <?php
-
-
 namespace Valued\WordPress;
-
 
 class PhpCompatibilityCheck {
     public static function isCompatible($plugin_name) {
-        if (version_compare(phpversion(), '7.0.0', '<')) {
-            add_action('admin_notices', function ($arguments) use ($plugin_name) {
-                $class = 'notice notice-error';
-                $message = sprintf(
-                    'The %s plugin is not compatible with PHP %s. Please upgrade to PHP 7.0.0 or higher',
-                    $plugin_name,
-                    phpversion()
-                );
-                printf('<div class="%s"><p>%s</p></div>', esc_attr($class), esc_html($message));
-            });
-            return false;
+        if (version_compare(PHP_VERSION, '7.0.0', '>=')) {
+            return true;
         }
-        return true;
+
+        add_action('admin_notices', function () use ($plugin_name) {
+            $class = 'notice notice-error';
+            $message = sprintf(
+                __('The %s plugin is not compatible with PHP %s. Please upgrade to PHP 7.0.0 or higher.', 'webwinkelkeur'),
+                $plugin_name,
+                PHP_VERSION
+            );
+            printf('<div class="%s"><p>%s</p></div>', esc_attr($class), esc_html($message));
+        });
+
+        return false;
     }
 }
