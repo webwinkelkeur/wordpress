2e914ad4b0e6b94cdad95d1614992db7b82e53e0
Albert Peschar <albert@peschar.net>
2021-01-27 16:18:19 +0200

    Support Yoast (#27)
    * Support Yoast
    
    * Change getActivePlugin function to non-static
    
    Co-authored-by: tsanko <tsankohadzhiev@gmail.com>

--- common/src/GtinHandler.php
+++ common/src/GtinHandler.php
@@ -3,31 +3,24 @@
 namespace Valued\WordPress;
 
 class GtinHandler {
-    const SUPPORTED_PLUGINS = [
-        'woosa-vandermeer/woosa-vandermeer.php' => ['vdm_ean'],
-        'woocommerce-product-feeds/woocommerce-gpf.php' => null,
-        'customer-reviews-woocommerce/ivole.php' => ['_cr_gtin'],
-        'product-gtin-ean-upc-isbn-for-woocommerce/product-gtin-ean-upc-isbn-for-woocommerce.php' => [
-            '_wpm_gtin_code', '_wpm_ean_code'
-        ],
-        'woo-product-feed-pro/woocommerce-sea.php' => ['_woosea_gtin', '_woosea_ean'],
-    ];
     const ATTRIBUTE_PREFIX = 'custom_attribute';
+
     const META_PREFIX = 'meta_key';
 
     private $product;
-    private $gtin_meta_key;
 
-    public function __construct(string $gtin_meta_key) {
-        $this->gtin_meta_key = $gtin_meta_key;
-    }
+    private $gtin_meta_key;
 
     public function setProduct(\WC_Product $product) {
         $this->product = $product;
     }
 
-    public static function getActivePlugin() {
-        foreach (self::SUPPORTED_PLUGINS as $plugin_name => $key) {
+    public function setGtinMetaKey(string $gtin_meta_key) {
+        $this->gtin_meta_key = $gtin_meta_key;
+    }
+
+    public function getActivePlugin() {
+        foreach ($this->getSupportedPlugins() as $plugin_name => $callback) {
             if (is_plugin_active($plugin_name)) {
                 return $plugin_name;
             }
@@ -35,28 +28,62 @@ class GtinHandler {
         return null;
     }
 
+    private function getSupportedPlugins(): array {
+        return [
+            'woosa-vandermeer/woosa-vandermeer.php' =>
+                $this->getFromMeta('vdm_ean'),
+            'woocommerce-product-feeds/woocommerce-gpf.php' =>
+                function () {
+                    if (function_exists('woocommerce_gpf_show_element')) {
+                        return (string) woocommerce_gpf_show_element('gtin', $this->product->post) ?: null;
+                    }
+                },
+            'customer-reviews-woocommerce/ivole.php' =>
+                $this->getFromMeta('_cr_gtin'),
+            'product-gtin-ean-upc-isbn-for-woocommerce/product-gtin-ean-upc-isbn-for-woocommerce.php' =>
+                $this->getFromMeta('_wpm_gtin_code', '_wpm_ean_code'),
+            'woo-product-feed-pro/woocommerce-sea.php' =>
+                $this->getFromMeta('_woosea_gtin', '_woosea_ean'),
+            'wpseo-woocommerce/wpseo-woocommerce.php' =>
+                function () {
+                    $meta_values = get_post_meta($this->product->get_id(), 'wpseo_global_identifier_values')[0] ?? null;
+                    foreach ([
+                        'gtin13',
+                        'gtin8',
+                        'gtin12',
+                        'gtin14',
+                        'isbn',
+                    ] as $gtin_key) {
+                        if (!empty($meta_values[$gtin_key])) {
+                            return (string) $meta_values[$gtin_key];
+                        }
+                    }
+                    return null;
+                },
+        ];
+    }
+
     public function getGtin(string $custom_gtin_key = null) {
         if (!empty($custom_gtin_key)) {
             return $this->getGtinFromKey($custom_gtin_key);
         }
-        if (function_exists('woocommerce_gpf_show_element')) {
-            return (string) woocommerce_gpf_show_element('gtin', $this->product->post) ?: null;
-        }
-        foreach (self::SUPPORTED_PLUGINS as $plugin_name => $keys) {
-            if ($keys && is_plugin_active($plugin_name)) {
-                return $this->getFromPluginMeta($keys);
+        foreach ($this->getSupportedPlugins() as $plugin_name => $callback) {
+            if (is_plugin_active($plugin_name)) {
+                return $callback();
             }
         }
         return $this->getGtinFromMeta($this->gtin_meta_key);
     }
 
-    private function getFromPluginMeta(array $keys) {
-        foreach ($keys as $key) {
-            if ($result = $this->getGtinFromMeta($key)) {
-                return $result;
+    private function getFromMeta(...$keys) {
+        return function () use ($keys) {
+            foreach ($keys as $key) {
+                if ($result = $this->getGtinFromMeta($key)) {
+                    return $result;
+                }
             }
-        }
-        return null;
+            return null;
+        };
     }
 
     private function getGtinFromMeta(string $key) {
