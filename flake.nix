372301b3f2a7a2ba877c7887b3513d45bf9921f3
Albert Peschar <albert@peschar.net>
2026-08-21 12:44:03 +0000

    Add Nix development shell

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Development environment for the WebwinkelKeur WordPress plugins";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [ pkgs.gettext ];
+          };
+        });
+    };
+}
